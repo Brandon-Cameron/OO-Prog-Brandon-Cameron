2642e3c0d0912fe848fcaadcb33197acb37b4795
(Student  C00260441) Brandon Cameron <C00260441@setu.ie>
2023-09-22 09:51:23 +0100

    Q1 and Q2 complete

---
+++ Lab Exercise 1/Lab Exercise 1/Q1-Q2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	//Q1
+	int array[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int *ptrArray = array;
+
+	for (int i = 0; i < sizeof(array) / sizeof(int); i++)
+	{
+		std::cout << *ptrArray + i << "\n";
+	}
+
+	std::cout << "sizeof(array) " << sizeof(array) << "\n";
+	std::cout << "sizeof(array) " << sizeof(ptrArray) << "\n";
+
+	//Q2
+	for (int& ref : array)
+	{
+		ref++;
+		std::cout << ref << "\n";
+	}
+
+	return 1;
+}
